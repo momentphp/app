d5bbea6394aaaca17bee715675c236d367e0985b
Grzesiek W <kontakt@kminek.pl>
2015-06-23 23:19:26 +0200

    changes

--- bundle/welcome/config/bundle.php
+++ bundle/welcome/config/bundle.php
@@ -2,7 +2,7 @@
 
 return [
     'welcome' => [
-        'title' => 'moment',
+        'title' => 'MomentPHP',
         'description' => 'Lightweight & intuitive PHP mini-framework enabling modular app design',
         'url' => [
             'github' => 'https://github.com/momentphp',
