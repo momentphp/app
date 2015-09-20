aa17c30c53a98b516da12f8366c94268ddb803e5
Grzesiek W <kontakt@kminek.pl>
2015-09-20 15:08:53 +0200

    changes

--- bundle/welcome/config/services.php
+++ bundle/welcome/config/services.php
@@ -4,14 +4,14 @@ return [
 
     'SmartyView' => [
         'name' => 'smartyView',
-        'template' => app('bundles')->paths('templates'),
+        'templates' => app('bundles')->paths('templates'),
         'compile' => path([app('pathTmp'), 'templates', 'smarty', app('bundles')->joinAliases()]),
         'autoescape' => false,
     ],
 
     'TwigView' => [
         'name' => 'twigView',
-        'template' => app('bundles')->paths('templates'),
+        'templates' => app('bundles')->paths('templates'),
         'compile' => path([app('pathTmp'), 'templates', 'twig', app('bundles')->joinAliases()]),
         'autoescape' => false,
     ]
