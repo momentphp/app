f4f09938ee7581b7454bed6231e9ea6fc16b1682
Grzesiek W <kontakt@kminek.pl>
2016-09-27 19:19:03 +0200

    email change

--- bundles/welcome/config/bundles.php
+++ bundles/welcome/config/bundles.php
@@ -8,7 +8,7 @@ return [
     'welcome' => [
         'title' => 'MomentPHP',
         'description' => 'The PHP mini-framework based on Slim and Laravel Components',
-        'email' => 'contact@momentphp.kminek.pl',
+        'email' => 'kontakt@kminek.pl',
         'url' => [
             'download' => 'https://github.com/momentphp/app/archive/master.zip',
             'github' => 'https://github.com/momentphp',
